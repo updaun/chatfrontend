389c55411cc0882811013da8d0205350524aed5d
updaun <updauney@daum.net>
2023-01-29 17:18:35 +0900

    chapter 22. React - props and state

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return <h2>React is interesting...</h2>;
-}
+const App = () => {
+
+  const [varA, setVarA] = useState("");
+  const [varB, setVarB] = useState("");
+
+  return <div>
+      <input
+        value={varA}
+        type="number"
+        onChange={(e) => {
+            setVarA(e.target.value);
+        }} 
+      />
+      <input
+        value={varB}
+        type="number"
+        onChange={(e) => {
+            setVarB(e.target.value);
+        }} 
+      />
+      <Result vA={varA} vB={varB} />
+  </div>;
+};
 
 export default App;
+
+const Result = (props) => {
+  const [result, setResult] = useState("");
+  return (
+    <div>
+      <h3>Result: {result}</h3>
+      <button onClick={() => {
+        var res = parseFloat(props.vA) + parseFloat(props.vB);
+        setResult(res);
+      }}
+      >
+        Calculate
+      </button>
+    </div>
+  );
+};
