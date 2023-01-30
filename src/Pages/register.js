e891a0135ce802d6d0cb4ebfecc489806a6fc1ae
updaun <updauney@daum.net>
2023-01-31 05:09:09 +0900

    chapter 29. Frontend Authentication

--- src/Pages/register.js
+++ src/Pages/register.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import {Link} from "react-router-dom";
-import {AuthForm} from "./login";
+import { AuthForm, loginRequest } from "./login";
+import { axiosHandler, errorHandler } from "../helper";
+import { REGISTER_URL } from "../urls";
 
-const Register = () => {
+const Register = (props) => {
+
+    const [registerData, setRegisterData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [error, setError] = useState();
+
+    const submit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+        const result = await axiosHandler({
+            method: "post",
+            url: REGISTER_URL,
+            data: registerData
+        }).catch(e => setError(errorHandler(e)));
+        if(result){
+            await loginRequest(registerData, setError, props);
+        }
+        setLoading(false);
+    }
+
+    const onChange = (e) => {
+        setRegisterData({
+            ...registerData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
     return (
     <div className="loginContainer">
         <div className="inner">
             <div className="logo">UPDAUN</div>
             <div className="title">Sign up</div>
 
-            <AuthForm />
+            <AuthForm
+                data={registerData}
+                onSubmit={submit}
+                onChange={onChange}
+                showPassword={showPassword}
+                error={error}
+                loading={loading}
+                setError={setError}
+                setShowPassword={setShowPassword} 
+            />
             
             <div className="switchOption">
                 Already got an account? <Link to="/login">Sign in</Link>
---
+++ src/urls.js
@@ -0,0 +1,4 @@
+const BASE_URL = "https://updaun.site/";
+
+export const LOGIN_URL = BASE_URL + "user/login";
+export const REGISTER_URL = BASE_URL + "user/register";
